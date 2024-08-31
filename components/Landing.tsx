4a09aa9231e19756e3ff66529ba3fa7c4e5f0a3a
CodeDiego15 <andrebermudezdiego@gmail.com>
2024-08-31 19:34:29 -0400

    commit

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,6 +1,6 @@
 import { Pixelify_Sans } from "next/font/google";
 import { Inter } from "next/font/google";
-
+import Image from "next/image";
 const pixelify_Sans = Pixelify_Sans({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,7 +8,7 @@ export default function Landing() {
   return (
  <div className={`min-h-screen text-white flex flex-col items-center justify-center pt-24 ${inter.className}`}>
     <div className="">
-      <img
+      <Image
         src="/megumi.jpeg" 
         alt="Avatar"
         className="rounded-full justify-center w-32 h-32 md:w-48 md:h-48 lg:w-56 lg:h-56"
@@ -17,7 +17,7 @@ export default function Landing() {
    
     <div className="text-center mt-4">
       <h1 className={`text-3xl md:text-5xl lg:text-5xl font-bold text-blue-500 mt-4 ${pixelify_Sans.className}`}>
-        Howdy! I'm /DiegoDev1/
+        Howdy! I&apos;m /DiegoDev1/
       </h1>
       <h2 className={`text-xl md:text-2xl lg:text-xl mt-2 ${pixelify_Sans.className}`}>
         Aspiring IT Security Engineer. I am Fifteen years old | Terminal tools developer | Network hacking
@@ -31,7 +31,7 @@ export default function Landing() {
     <div className="bg-neutral-800/90 p-4 mt-8 rounded-lg max-w-xs sm:max-w-md lg:max-w-2xl text-center">
       <h3 className={`text-lg md:text-xl lg:text-2xl font-semibold ${pixelify_Sans.className}`}>About me</h3>
       <p className={`mt-2 text-sm md:text-base lg:text-lg font-extralight ${inter.className}`}>
-        I'm a passionate fullstack developer with a deep love for anime and all things tech. My favorite programming language is Go, but I also dive into various other technologies with enthusiasm. I have a strong interest in cybersecurity and ethical hacking, constantly exploring new ways to secure systems and uncover vulnerabilities. When I'm not coding, you can find me engrossed in the latest anime series or experimenting with new security tools. Let's create something amazing together, whether it's crafting secure systems or building powerful applications!
+        I&apos;m a passionate fullstack developer with a deep love for anime and all things tech. My favorite programming language is Go, but I also dive into various other technologies with enthusiasm. I have a strong interest in cybersecurity and ethical hacking, constantly exploring new ways to secure systems and uncover vulnerabilities. When I'm not coding, you can find me engrossed in the latest anime series or experimenting with new security tools. Let's create something amazing together, whether it's crafting secure systems or building powerful applications!
       </p>
     </div>
   </div>
