d471654a69c7eb89c87a3a1b45b8dfe011a29391
CodeDiego15 <andrebermudezdiego@gmail.com>
2024-09-01 22:03:38 -0400

    commit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,18 @@
-
+import React, { useState } from 'react';
 import AnimatedBackground from "@/components/BackgroundAnimated";
 import Header from "@/components/Header";
 import Landing from "@/components/Landing";
 import Skills from "@/components/Skills";
 import Work from "@/components/Work"
 import Projects from "@/components/Projects"
-export default function Home() {
-  return (
 
-    <AnimatedBackground>
-        <Header />
-        <Projects />
+const Home: React.FC = () => {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  return (
+    <AnimatedBackground isAnimating={isAnimating}>
+      <Header setIsAnimating={setIsAnimating} />
+      <Projects />
     </AnimatedBackground>
-  ); 
-}
+  );
+};
