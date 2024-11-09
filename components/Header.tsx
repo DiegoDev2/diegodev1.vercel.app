49543e5eb50757a3a9af162cb168837ebb1575a1
DiegoDev <DiegoDev2@users.noreply.github.com>
2024-11-09 01:00:49 -0400

    refactor

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="fixed top-8 left-1/2 transform -translate-x-1/2 z-50 w-[100%] max-w-[1100px] border border-neutral-800 bg-[#1a1a1a] bg-opacity-70 backdrop-blur-lg rounded-2xl px-8 py-1 shadow-md flex justify-between items-center">
+    <header className="fixed top-8 left-1/2 transform -translate-x-1/2 z-50 w-[100%] max-w-[1100px] border border-neutral-800 bg-[#1a1a1a] bg-opacity-70 backdrop-blur-lg rounded-2xl px-8 py-2 shadow-md flex justify-between items-center">
       <div className="flex items-center">
         <Image
           src="/Icon.jpg"
@@ -13,7 +13,7 @@ export default function Header() {
         />
       </div>
 
-      <nav className="flex gap-10 font-extralight text-base text-gray-300">
+      <nav className="flex gap-10 font-extralight text-base text-gray-400">
         <a href="#home" className="hover:text-white transition-color">Home</a>
         <a href="#work" className="hover:text-white transition-colors">Work</a>
         <a href="#resume" className="hover:text-white transition-colors">Resume</a>
