748ef775a19565ef3dc78e83c3230a44bec476b4
DiegoDev2 <andrebermudezdiego@gmail.com>
2025-06-14 15:02:36 -0400

    Add dynamic metadata to blog post pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,12 +1,26 @@
 import { sanityClient } from "@/lib/sanity.client";
 import { notFound } from "next/navigation";
 import { PostBody } from "@/components/ui/PostBody";
+import { type Metadata } from "next";
 
-export default async function BlogPost({
-  params,
-}: {
-  params: { slug: string };
-}) {
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const post = await sanityClient.fetch(
+    `*[_type == "post" && slug.current == $slug][0]{ title }`,
+    { slug: params.slug },
+  );
+
+  return {
+    title: post?.title ?? "Post",
+  };
+}
+
+export default async function BlogPost({ params }: Props) {
   const post = await sanityClient.fetch(
     `*[_type == "post" && slug.current == $slug][0]{
       title,
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import Landing from "@/components/Landing";
 import ProjectsSection from "@/components/Projects";
 import Process from "@/components/Process";
@@ -9,7 +8,7 @@ export default function Home() {
   return (
     <>
       <section className="section-down blur-progressive"></section>
-      <Header />
+
       <LenisProvider />
 
       <Landing />
